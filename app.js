99b73f8ebbaf5584e1338e56199ac6b763f73135
root <root@ip-172-31-82-146.ec2.internal>
2024-08-24 10:11:25 +0000

    this is change be Dev

--- app.js
+++ app.js
@@ -8,7 +8,7 @@ const port = 3030;
 const server = http.createServer((req, res) => {
   res.statusCode = 200;
   res.setHeader('Content-Type', 'text/plain');
-  res.end('Hello World this is ashi\n');
+  res.end('Hello World this is not only ashi also her friend Devavrat who automatd with termination\n');
 });
 
 // Listen on port 3000 and IP 127.0.0.1.
